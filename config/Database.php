819d36e70e09f40d73be566a6b93502433ffe295
Kadmiel Herbert <kadmieleherbert@gmail.com>
2024-03-20 19:19:30 -0400

    Database.php update

--- config/Database.php
+++ config/Database.php
@@ -7,6 +7,7 @@ class Database {
     private $username;
     private $password;
     private $conn;
+    private $db_connection;
 
     public function __construct() {
         $this->username = getenv('USERNAME');
@@ -14,6 +15,7 @@ class Database {
         $this->db_name = getenv('DBNAME');
         $this->host = getenv('HOST');
         $this->port = getenv('PORT');
+        $this->db_connectrion = getenv('DBConnection');
     }
 
     public function connect() {
@@ -23,10 +25,11 @@ class Database {
         } else {
 
         }
-        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+        //$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
 
         try {
-            $this->conn = new PDO ($dsn, $this->username, $this->password);
+            //$this->conn = new PDO ($dsn, $this->username, $this->password);
+            $this->conn = new PDO (db_connectrion);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
